d750477607a2ff030a52ab99ff7436da71397e3f
Lawrence Ikhidero <lawrence@ikhidero.com>
2024-04-19 20:41:45 +0100

    basic auth

---
+++ 0x01-Basic_authentication/README.md
@@ -0,0 +1,19 @@
+# 0x01. Basic Authentication
+
+## Introduction
+This project explores the concept of basic authentication, including what authentication means, Base64 encoding, and how to implement basic authentication using the Authorization header.
+
+## Authentication
+Authentication is the process of verifying the identity of a user or entity. It ensures that the user or entity is who they claim to be before granting access to resources or services.
+
+## Base64 Encoding
+Base64 is a binary-to-text encoding scheme that converts binary data into ASCII characters. It is commonly used to encode binary data, such as images or cryptographic keys, into a format that can be safely transmitted over text-based protocols, such as HTTP.
+
+## Encoding a String in Base64
+To encode a string in Base64, you can use built-in functions or libraries available in most programming languages. For example, in Python, you can use the `base64` module:
+
+```python
+import base64
+
+# Encode a string in Base64
+encoded_string =
---
+++ api/v1/auth/auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Authentication module for v1 API.
+"""
+from flask import request
+from typing import List, TypeVar
+import re
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if a request to a given path requires authentication or not.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Returns the value of the Authorization header in the given request
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the currently authenticated user based on the provided request
+        """
+        return None
